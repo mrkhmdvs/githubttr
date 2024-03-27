ff266a0a3a02d8002f574c6a342dce203adb0ef0
Muxiddin <xardinscott@gmail.com>
2024-03-28 01:10:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let num=5;
+console.log(num);
+
+
+// const favFruit=[];
+// favFruit[0]=prompt('whats ur fav fruit',"")
+// favFruit[1]=prompt('whats ur fav fruit',"")
+// favFruit[2]=prompt('whats ur fav fruit',"")
+// favFruit[3]=prompt('whats ur fav fruit',"")
+// favFruit[4]=prompt('whats ur fav fruit',"")
+// favFruit[5]=prompt('whats ur fav fruit',"")
+// favFruit[6]=prompt('whats ur fav fruit',"")
+// favFruit[7]=prompt('whats ur fav fruit',"")
+// favFruit[8]=prompt('whats ur fav fruit',"")
+// favFruit[9]=prompt('whats ur fav fruit',"")
+// favFruit[10]=prompt('whats ur fav frui',"")
+// console.log(favFruit)
+
+
+const channel=prompt("whats ur fav youtuber");
+console.log("https://youtube.com/"+ channel);
+console.log("https://youtube.com/"+ channel+'/videos');
